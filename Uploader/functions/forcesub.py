8d163e327ba7810ef1e3ce627e0c4a156d0f649d
jagan1857 <107793234+jagan1857@users.noreply.github.com>
2022-08-19 12:09:58 +0530

    Update forcesub.py

--- Uploader/functions/forcesub.py
+++ Uploader/functions/forcesub.py
@@ -15,7 +15,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/tellybots).",
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/My_Test_Botz).",
                 
                 disable_web_page_preview=True,
                 reply_to_message_id=message.id,
@@ -39,7 +39,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/Tellybots).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/My_Test_Botz).",
             
             disable_web_page_preview=True,
             reply_to_message_id=message.id,
