29ac12a80983a01b3e96b83cb720cfcb2615c33e
jagan1857 <107793234+jagan1857@users.noreply.github.com>
2022-09-07 20:09:03 +0530

    Update script.py

--- Uploader/script.py
+++ Uploader/script.py
@@ -11,7 +11,7 @@ Hi {} 🥰
 
 Use help button to know how to use me
 
-🦊 <b>Maintained By</b> : [Botz Hub](https://telegram.me/TellyBots)
+🦊 <b>Maintained By</b> : [Botz Hub](https://t.me/My_Test_Botz)
 """
     HELP_TEXT = """
 You need Help ?? 😅
@@ -32,13 +32,11 @@ Maintained By : [Botz Hub](https://t.me/My_Test_Botz)
  
 """
     ABOUT_TEXT = """
-**♻️ My Name** : [URL UPLODER BOT Bot](https://t.me/URL_UPLODER_MB_BOT)
+**♻️ My Name** : [URL UPLODER BOT Bot](https://t.me/Url_Uploader_Pro_Robot)
 
 **🌀 Channel** : [Botz Hub](https://t.me/My_Test_Botz)
 
-**⚗️ Version** : [4.0 Beta](https://t.me/TellyUploaderRobot)
-
-**🌠 Source** : [Click Here](https://t.me/tellybots_digital)
+**⚗️ Version** : [4.0 Beta](https://t.me/Url_Uploader_Pro_Robot)
 
 **🌺 Heroku** : [Heroku](https://heroku.com/)
 
@@ -46,7 +44,7 @@ Maintained By : [Botz Hub](https://t.me/My_Test_Botz)
 
 **🇵🇲 Framework :** [Pyrogram 2.0.30](https://docs.pyrogram.org/)
 
-**👲 Developer :** [Botz Hub](https://t.me/My_Test_Botz)
+**👲 Developer :** [MR_JAGANMOHAN](https://t.me/MR_JAGANMOHAN)
 
 **🦊 Maintained By :** [Botz Hub](https://t.me/My_Test_Botz)
 
@@ -148,14 +146,14 @@ Maintained By : [Botz Hub](https://t.me/My_Test_Botz)
     ANNO_UPLOAD = " UploadinG📤 \n\n To  anonfiles.com "
     BAY_UPLOAD = " UploadinG📤 \n\n To  bayfiles.com "
     GO_FILE_UPLOAD = " 📤UploadinG📤 \n\n To  gofile.io "
-    DOWNLOAD_START = "Trying to Download ⌛\n\n💮🌸 <i>{} 💮🌸</i>"
-    UPLOAD_START = "💮🌸 <i>{} 💮🌸</i>\n\n📤 Uploading Please Wait "
+    DOWNLOAD_START = "Trying to Download ⌛\n\n⚡⚡ <i>{} ⚡⚡</i>"
+    UPLOAD_START = "⚡⚡ <i>{} ⚡⚡</i>\n\n📤 Uploading Please Wait "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = " JOIN : https://t.me/TGBotsCollection\nFor the List of Telegram Bots"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = " JOIN : https://t.me/My_Test_Botz\nFor More of Telegram Bots"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Dᴏᴡɴʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs.\n\nTʜᴀɴᴋs Fᴏʀ Usɪɴɢ Mᴇ\n\nUᴘʟᴏᴀᴅᴇᴅ ɪɴ {} sᴇᴄᴏɴᴅs"
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/MR_JAGANMOHAN'>@MR_JAGANMOHAN</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Cᴜsᴛᴏᴍ ᴠɪᴅᴇᴏ / ғɪʟᴇ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ. Tʜɪs ɪᴍᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴜsᴇᴅ ɪɴ ᴛʜᴇ ᴠɪᴅᴇᴏ / ғɪʟᴇ."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Cᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴄʟᴇᴀʀᴇᴅ sᴜᴄᴄᴇsғᴜʟʟʏ"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
@@ -170,10 +168,10 @@ Maintained By : [Botz Hub](https://t.me/My_Test_Botz)
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /ren with custom thumbnail support"
-    AFTER_GET_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nJoin : @Tellybots_4u"
+    AFTER_GET_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nJoin : @My_Test_Botz"
     AFTER_GET_DL_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nValid for <b>14</b> days.\nJoin : @Tellybots_4u"
-    #AFTER_GET_DL_LINK = " {} valid for 30 or more days.\n\n Join : @Tellybots_4u \n For the list of Telegram bots. "
-    AFTER_GET_GOFILE_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n<b>File MD5 Checksum :</b> <code>{}</code>\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\n Valid untill 10 days of inactivity\nJoin : @TGBotsCollection"
+    #AFTER_GET_DL_LINK = " {} valid for 30 or more days.\n\n Join : @My_Test_Botz \n For More of Telegram bots. "
+    AFTER_GET_GOFILE_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n<b>File MD5 Checksum :</b> <code>{}</code>\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\n Valid untill 10 days of inactivity\nJoin : @My_Test_Botz"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS for screenshot of that specific time."""
     FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
@@ -184,11 +182,11 @@ Maintained By : [Botz Hub](https://t.me/My_Test_Botz)
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @TGBotsCollectionbot \n For the list of Telegram bots. "
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @My_Test_Botz \n For the list of Telegram bots. "
     EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
     EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
     UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://t.me/My_Test_Botz'>@MR_JAGANMOHAN</a>"
     EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
 You can use /rename command after receiving file to rename it with custom thumbnail support."""
     CANCEL_STR = "Process Cancelled"
